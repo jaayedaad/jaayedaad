9e5e07926f0c26519e1af9a53878a1edef09da40
Siddharth Movaliya <siddraj1992@gmail.com>
2024-02-22 21:41:35 +0530

    chore: remove vehicle category

--- constants/category.ts
+++ constants/category.ts
@@ -1,6 +1,5 @@
 export const categories = [
   { value: "PROPERTY", label: "PROPERTY" },
   { value: "JEWELLERY", label: "JEWELLERY" },
-  { value: "VEHICLE", label: "VEHICLE" },
   { value: "OTHERS", label: "OTHERS" },
 ];
