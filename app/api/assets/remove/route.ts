9c56187543479ca73fa5c895b5f02d4b99ef7a7f
Siddharth Movaliya <siddraj1992@gmail.com>
2024-03-03 15:57:45 +0530

    fix: remove asset API route

--- app/api/assets/remove/route.ts
+++ app/api/assets/remove/route.ts
@@ -1,10 +1,14 @@
 import { prisma } from "@/lib/prisma";
 export async function POST(req: Request) {
-  const { assetId } = await req.json();
-  if (assetId) {
-    await prisma.asset.delete({
-      where: { id: assetId },
-    });
+  try {
+    const assetId = await req.json();
+    if (assetId) {
+      await prisma.asset.delete({
+        where: { id: assetId },
+      });
+      return Response.json({ success: "Asset removed successfully!" });
+    }
+  } catch {
+    return Response.json({ error: "Error Removing asset!" });
   }
-  return new Response("OK");
 }
