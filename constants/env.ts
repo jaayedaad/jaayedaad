c709389d535ff80be7df5787c1deea10ba936c54
ShubhamPalriwala <spalriwalau@gmail.com>
2024-04-08 00:00:20 +0530

    fix: parse sia api url only if it actually exists

--- constants/env.ts
+++ constants/env.ts
@@ -1,6 +1,9 @@
 export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 export const DATABASE_URL = process.env.DATABASE_URL;
-export const SIA_API_URL = process.env.SIA_API_URL;
+export const SIA_API_URL =
+  process.env.SIA_API_URL && process.env.SIA_API_URL.length > 0
+    ? process.env.SIA_API_URL
+    : undefined;
 export const SIA_ADMIN_USERNAME = process.env.SIA_ADMIN_USERNAME;
 export const SIA_ADMIN_PASSWORD = process.env.SIA_ADMIN_PASSWORD;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
