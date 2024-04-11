3f9021dc2dc434ae01731aff1a921cf0127676c7
Siddharth Movaliya <siddraj1992@gmail.com>
2024-04-11 19:44:16 +0530

    fix: sell route to work properly when SIA is not configured

--- app/api/assets/sell/route.ts
+++ app/api/assets/sell/route.ts
@@ -53,20 +53,10 @@ export async function PUT(req: Request) {
   });
 
   if (user) {
-    const username = SIA_ADMIN_USERNAME;
-    const password = SIA_ADMIN_PASSWORD;
-    const basicAuth =
-      "Basic " + Buffer.from(username + ":" + password).toString("base64");
-
     const encryptionKey =
       user.id.slice(0, 4) + ENCRYPTION_KEY + user.id.slice(-4);
     let ownedAsset: AssetWithTransaction[] | TAsset[] | undefined;
-    if (USE_SIA) {
-      const userAssets = await getAllAssets();
-      ownedAsset = userAssets?.filter(
-        (asset) => asset.name === sellRequest.name
-      );
-    }
+
     if (DATABASE_URL) {
       const userAssets = decryptObjectValues(
         user.assets,
@@ -75,7 +65,13 @@ export async function PUT(req: Request) {
       ownedAsset = userAssets.filter(
         (asset) => asset.name === sellRequest.name
       );
+    } else if (USE_SIA) {
+      const userAssets = await getAllAssets();
+      ownedAsset = userAssets?.filter(
+        (asset) => asset.name === sellRequest.name
+      );
     }
+
     if (ownedAsset && ownedAsset.length > 0) {
       // Get the required asset
       const assets = ownedAsset;
@@ -84,127 +80,29 @@ export async function PUT(req: Request) {
         let sellQuantity: string = sellRequest.quantity;
 
         for (const asset of assets) {
-          // Parsing decrypted data
-          const assetToSell = await getAssetById(user.id, asset.id);
-
           // set asset quantity to 0 if sell quantiy is same as asset quantity
           if (+asset.quantity - +sellQuantity == 0) {
-            if (USE_SIA) {
-              // update asset
-              await fetch(
-                `${SIA_API_URL}/worker/objects/${user.id}/assets/${assetToSell.id}/data`,
-                {
-                  method: "PUT",
-                  headers: {
-                    Authorization: basicAuth,
-                  },
-                  body: JSON.stringify({
-                    data: CryptoJS.AES.encrypt(
-                      JSON.stringify({
-                        ...assetToSell,
-                        quantity: "0",
-                      }),
-                      encryptionKey
-                    ).toString(),
-                  }),
-                }
-              );
-            }
-            if (DATABASE_URL) {
-              // update asset
-              await prisma.asset.update({
-                where: {
-                  id: asset.id,
-                },
-                data: {
-                  quantity: encryptDataValue("0", encryptionKey),
-                },
-              });
-            }
+            await updateAssetQuantity(user.id, asset.id, "0", encryptionKey);
           }
           // Update asset quantity if asset quantity is greater than sell quantity
-          else if (+asset.quantity - +sellQuantity > 0) {
-            if (USE_SIA) {
-              // update asset's quantity
-              await fetch(
-                `${SIA_API_URL}/worker/objects/${user.id}/assets/${assetToSell.id}/data`,
-                {
-                  method: "PUT",
-                  headers: {
-                    Authorization: basicAuth,
-                  },
-                  body: JSON.stringify({
-                    data: CryptoJS.AES.encrypt(
-                      JSON.stringify({
-                        ...assetToSell,
-                        quantity: (+asset.quantity - +sellQuantity).toString(),
-                      }),
-                      encryptionKey
-                    ).toString(),
-                  }),
-                }
-              );
-            }
-            if (DATABASE_URL) {
-              // update asset's quantity
-              await prisma.asset.update({
-                where: {
-                  id: asset.id,
-                },
-                data: {
-                  quantity: encryptDataValue(
-                    (+asset.quantity - +sellQuantity).toString(),
-                    encryptionKey
-                  ),
-                },
-              });
-            }
+          else {
+            const newQuantity = (+asset.quantity - +sellQuantity).toString();
+            await updateAssetQuantity(
+              user.id,
+              asset.id,
+              newQuantity,
+              encryptionKey
+            );
           }
 
           // make transaction
-          if (USE_SIA) {
-            // make transaction
-            await fetch(
-              `${SIA_API_URL}/worker/objects/${user.id}/assets/${assetToSell.id}/transactions/${transactionId}`,
-              {
-                method: "PUT",
-                headers: {
-                  Authorization: basicAuth,
-                },
-                body: JSON.stringify({
-                  data: CryptoJS.AES.encrypt(
-                    JSON.stringify({
-                      id: transactionId,
-                      date: sellRequest.date,
-                      quantity: sellQuantity,
-                      price: sellRequest.price,
-                      type: "sell",
-                      assetId: assetToSell.id,
-                    }),
-                    encryptionKey
-                  ).toString(),
-                }),
-              }
-            );
-          }
-          if (DATABASE_URL) {
-            // encrypt data
-            const encryptedData: Transaction = encryptObjectValues(
-              {
-                id: transactionId,
-                date: sellRequest.date,
-                quantity: sellQuantity,
-                price: sellRequest.price,
-                type: "sell",
-                assetId: asset.id,
-              },
-              encryptionKey
-            );
-            // make transaction
-            await prisma.transaction.create({
-              data: encryptedData,
-            });
-          }
+          await makeTransaction(
+            user.id,
+            asset.id,
+            sellRequest,
+            transactionId,
+            encryptionKey
+          );
         }
 
         return Response.json({ success: "Successfully sold asset" });
@@ -217,4 +115,113 @@ export async function PUT(req: Request) {
   }
 
   return new Response("OK");
+}
+
+async function updateAssetQuantity(
+  userId: string,
+  assetId: string,
+  newQuantity: string,
+  encryptionKey: string
+) {
+  if (USE_SIA) {
+    const basicAuth =
+      "Basic " +
+      Buffer.from(`${SIA_ADMIN_USERNAME}:${SIA_ADMIN_PASSWORD}`).toString(
+        "base64"
+      );
+    const assetToSell = await getAssetById(userId, assetId);
+    // update asset's quantity
+    await fetch(
+      `${SIA_API_URL}/worker/objects/${userId}/assets/${assetToSell.id}/data`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: basicAuth,
+        },
+        body: JSON.stringify({
+          data: CryptoJS.AES.encrypt(
+            JSON.stringify({
+              ...assetToSell,
+              quantity: newQuantity,
+            }),
+            encryptionKey
+          ).toString(),
+        }),
+      }
+    );
+  }
+  if (DATABASE_URL) {
+    // update asset's quantity
+    await prisma.asset.update({
+      where: {
+        id: assetId,
+      },
+      data: {
+        quantity: encryptDataValue(newQuantity, encryptionKey),
+      },
+    });
+  }
+}
+
+async function makeTransaction(
+  userId: string,
+  assetId: string,
+  sellRequest: {
+    name: string;
+    quantity: string;
+    price: string;
+    date: string;
+  },
+  transactionId: string,
+  encryptionKey: string
+) {
+  if (USE_SIA) {
+    const basicAuth =
+      "Basic " +
+      Buffer.from(`${SIA_ADMIN_USERNAME}:${SIA_ADMIN_PASSWORD}`).toString(
+        "base64"
+      );
+    const assetToSell = await getAssetById(userId, assetId);
+    // make transaction
+    await fetch(
+      `${SIA_API_URL}/worker/objects/${userId}/assets/${assetToSell.id}/transactions/${transactionId}`,
+      {
+        method: "PUT",
+        headers: {
+          Authorization: basicAuth,
+        },
+        body: JSON.stringify({
+          data: CryptoJS.AES.encrypt(
+            JSON.stringify({
+              id: transactionId,
+              date: sellRequest.date,
+              quantity: sellRequest.quantity,
+              price: sellRequest.price,
+              type: "sell",
+              assetId: assetToSell.id,
+            }),
+            encryptionKey
+          ).toString(),
+        }),
+      }
+    );
+  }
+  if (DATABASE_URL) {
+    // encrypt data
+    const encryptedData: Transaction = encryptObjectValues(
+      {
+        id: transactionId,
+        date: sellRequest.date,
+        quantity: sellRequest.quantity,
+        price: sellRequest.price,
+        type: "sell",
+        assetId: assetId,
+      },
+      encryptionKey
+    );
+    // make transaction
+    await prisma.transaction.create({
+      data: encryptedData,
+    });
+  }
 }
