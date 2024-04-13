3cf6c27919a7b69429494b427015807cfd9e7c01
ShubhamPalriwala <spalriwalau@gmail.com>
2024-04-13 13:24:16 +0530

    chore: move helper fns in one place

--- app/(protected)/dashboard/actions.ts
+++ app/(protected)/dashboard/actions.ts
@@ -1,9 +1,8 @@
 "use server";
 
-import { ENCRYPTION_KEY } from "@/constants/env";
 import { authOptions } from "@/lib/authOptions";
-import { decryptObjectValues } from "@/lib/dataSecurity";
-import { TAsset, TTwelveDataResult } from "@/lib/types";
+import { findTopMatchingAssets, transformToResultFormat } from "@/lib/helper";
+import { TTwelveDataResult } from "@/lib/types";
 import { getDeccryptedAssetsByUserId } from "@/services/asset";
 import { getServerSession } from "next-auth";
 
@@ -25,46 +24,3 @@ export const searchResultsFromExistingAssetsInDatabaseAction = async (
 
   return transformToResultFormat(topMatchingAssets);
 };
-
-// Function to calculate similarity between search query and asset name
-function similarityScore(assetName: string, searchString: string) {
-  const cleanAssetName = assetName.toLowerCase().trim();
-  const cleanSearchString = searchString.toLowerCase().trim();
-
-  // Check if the asset name includes the search query
-  return cleanAssetName.includes(cleanSearchString) ? 100 : 0;
-}
-
-function findTopMatchingAssets(assets: any, searchString: string) {
-  // Calculate similarity between searchString and asset names
-  const matchingAssets = assets.map((asset: any) => {
-    const similarity = similarityScore(asset.name, searchString);
-    return { ...asset, similarity };
-  });
-
-  // Filter out assets with 0 similarity
-  const filteredAssets = matchingAssets.filter(
-    (asset: any) => asset.similarity > 0
-  );
-
-  // Sort assets based on similarity score
-  filteredAssets.sort((a: any, b: any) => b.similarity - a.similarity);
-
-  // Return top matching asset if any
-  return filteredAssets.slice(0, 1);
-}
-
-function transformToResultFormat(
-  topMatchingAssets: (TAsset & { similarity: number })[]
-) {
-  return topMatchingAssets.map((asset) => ({
-    instrument_name: asset.name,
-    symbol: asset.symbol || "", // If symbol is null, set it to an empty string
-    instrument_type: asset.category,
-    exchange: asset.exchange || "", // If exchange is null, set it to an empty string
-    mic_code: "", // Not available in the database
-    currency: asset.buyCurrency,
-    country: "", // Not available in the database
-    exchange_timezone: "", // Not available in the database
-  }));
-}
--- lib/helper.ts
+++ lib/helper.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { TAsset } from "./types";
 
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
@@ -21,3 +22,46 @@ export const capitalize = (inputStr: string) => {
   if (!inputStr) return "";
   return inputStr.charAt(0).toUpperCase() + inputStr.slice(1);
 };
+
+// Function to calculate similarity between search query and asset name
+export function similarityScore(assetName: string, searchString: string) {
+  const cleanAssetName = assetName.toLowerCase().trim();
+  const cleanSearchString = searchString.toLowerCase().trim();
+
+  // Check if the asset name includes the search query
+  return cleanAssetName.includes(cleanSearchString) ? 100 : 0;
+}
+
+export function findTopMatchingAssets(assets: any, searchString: string) {
+  // Calculate similarity between searchString and asset names
+  const matchingAssets = assets.map((asset: any) => {
+    const similarity = similarityScore(asset.name, searchString);
+    return { ...asset, similarity };
+  });
+
+  // Filter out assets with 0 similarity
+  const filteredAssets = matchingAssets.filter(
+    (asset: any) => asset.similarity > 0
+  );
+
+  // Sort assets based on similarity score
+  filteredAssets.sort((a: any, b: any) => b.similarity - a.similarity);
+
+  // Return top matching asset if any
+  return filteredAssets.slice(0, 1);
+}
+
+export function transformToResultFormat(
+  topMatchingAssets: (TAsset & { similarity: number })[]
+) {
+  return topMatchingAssets.map((asset) => ({
+    instrument_name: asset.name,
+    symbol: asset.symbol || "", // If symbol is null, set it to an empty string
+    instrument_type: asset.category,
+    exchange: asset.exchange || "", // If exchange is null, set it to an empty string
+    mic_code: "", // Not available in the database
+    currency: asset.buyCurrency,
+    country: "", // Not available in the database
+    exchange_timezone: "", // Not available in the database
+  }));
+}
